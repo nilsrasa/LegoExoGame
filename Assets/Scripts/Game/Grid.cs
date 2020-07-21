0c70dd7152b028630778570de0699bf4782b07ca
nilsras <s165526@student.dtu.dk>
2020-07-21 12:33:33 +0200

    Grid class

---
+++ Assets/Scripts/Game/Grid.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    [SerializeField] private GameObject _rightLane, _leftLane, _topLane, _bottomLane;
+
+    public void Init(float width, float depth)
+    {
+        _rightLane.transform.localPosition = Vector3.right * width;
+        _leftLane.transform.localPosition = Vector3.left * width;
+        _topLane.transform.localPosition = Vector3.up * width;
+        _bottomLane.transform.localPosition = Vector3.down * width;
+
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
